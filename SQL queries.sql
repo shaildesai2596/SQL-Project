30683ebf3c7049dcbd81757b9a31db5ac4a459db
shaildesai2596 <96824524+shaildesai2596@users.noreply.github.com>
2022-01-05 17:25:54 +0530

    Add files via upload

---
+++ SQL queries.sql
@@ -0,0 +1,65 @@
+--top 5 sales and their respective customers
+
+select top 5 [Sales], customername from sales_data_sample 
+order by sales desc ;
+
+--top 10 quantity ordered and their respective customers
+
+select top 10 [quantityordered], customername from sales_data_sample 
+order by quantityordered desc ;
+
+--total sales qtr wise
+
+select qtr_id, sum(sales) as total_sales from sales_data_sample 
+group by QTR_ID
+order by sum(sales) desc;
+
+--total sales qtr wise + product line wise
+
+select qtr_id, productline, sum(sales) as total_sales from sales_data_sample 
+group by QTR_ID, productline
+order by sum(sales) desc;
+
+--top 5 max sales on a single date
+
+select top 5 sum(sales) as Max_Sales_On_Single_Day, customername, cast(ORDERDATE as date) as Date from sales_data_sample 
+group by ORDERDATE
+order by sum(sales) desc;
+
+--(HAVING ISSUE WITH MONTH, NOT GETTING 11 AS THE MONTH AND GETTING 00 AT ITS PLACE)
+--(HOW TO ADD CUSTOMER NAMES IN ONE ROW ALL TOGETHER FOR ABOVE QUERY)
+
+--product line with least sales
+
+select sum(sales) as Least_Sales, PRODUCTLINE from sales_data_sample
+group by PRODUCTLINE
+order by sum(sales) asc;
+
+--check duplication in orders
+
+select *, ROW_NUMBER() over
+(
+partition by 
+ordernumber
+order by
+ordernumber
+)
+from sales_data_sample;
+
+
+--top 5 customers with maximum orders
+
+select customername, count(customername) as count from sales_data_sample
+group by CUSTOMERNAME
+order by count(CUSTOMERNAME) desc;
+
+--top 5 orders with large deal size
+
+select top 5 * from sales_data_sample
+where [DEALSIZE] = 'large';
+
+--countries with least orders
+
+select count(*), COUNTRY from sales_data_sample
+group by country
+order by count(*);
---
+++ sql queries.sql
@@ -0,0 +1,53 @@
+--create new column of sales
+
+update ['Sales Orders Sheet$']
+set sales = [Unit Price] * [Order Quantity];
+
+--sales channel wise 
+
+select [Sales Channel], format(sum(sales), '#,#') as Total_Sales from ['Sales Orders Sheet$']
+group by [Sales Channel];
+
+
+--gap between procured_date and order_date
+
+update ['Sales Orders Sheet$']
+set [diff_between_procureddate_orderdate] = DATEDIFF(MONTH, ['Sales Orders Sheet$'].ProcuredDate, ['Sales Orders Sheet$']. OrderDate);
+
+select ['Sales Orders Sheet$'].ProcuredDate, ['Sales Orders Sheet$'].OrderDate, [diff_between_procureddate_orderdate]
+from ['Sales Orders Sheet$'];
+
+--DAYS NOT GETTING DISPLAYED SO MONTH IS TAKEN AS AN ARGUMENT IN DATEDIFF
+
+--top 5 highest discount given to which customers
+
+select top 5 ([Discount Applied]), ['Sales Orders Sheet$']._CustomerID, [Customer Names] 
+from ['Sales Orders Sheet$'] join
+[dbo].['Customers Sheet$']
+on ['Sales Orders Sheet$']. _CustomerID = ['Customers Sheet$']. _CustomerID
+order by ([Discount Applied]) desc
+
+--maximum sales of which products
+--channel wise which product is sold the maximum (quantity wise)
+
+alter table [dbo].['Sales Orders Sheet$']
+add quantity_sold int;
+
+update ['Sales Orders Sheet$']
+set [quantity_sold]= (select sum([Order Quantity]) from ['Sales Orders Sheet$']
+group by _ProductID, [Sales Channel]);
+
+--qtr wise sales
+
+select DATEPART(YEAR, [OrderDate]) as year, DATEPART(quarter, [OrderDate]) as quarter, sum(sales) as sales from ['Sales Orders Sheet$']
+group by DATEPART(YEAR, [OrderDate]), DATEPART(quarter, [OrderDate])
+order by 1,2;
+
+--year wise sales
+
+select DATEPART(YEAR, [OrderDate]) as year, sum(sales) as sales from ['Sales Orders Sheet$']
+group by DATEPART(YEAR, [OrderDate])
+order by 1;
+
+--state wise sales
+
